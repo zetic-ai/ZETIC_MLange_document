f092bd9684a74d6cc5d3fe9d14ed017e0139929a
Yeonseok Kim <yeonseok@zetic.ai>
2024-08-05 07:38:52 +0000

    Update documents with new hotjar function

--- docs/_static/insert_hotjar.js
+++ docs/_static/insert_hotjar.js
@@ -1,4 +1,34 @@
-// _static/insert_gtag.js
+// _static/insert_hotjar.js
+
+// Function to add a script element to the head
+function addScript(src, onload) {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = src;
+    if (onload) {
+        script.onload = onload;
+    }
+    document.head.appendChild(script);
+}
+
+// Add Google Tag Manager
+addScript('https://www.googletagmanager.com/gtag/js?id=G-S2NQCWGG1N', function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-S2NQCWGG1N');
+});
+
+// Add Hotjar
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:YOUR_HOTJAR_ID,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
 (function() {
     var gtagScript = document.createElement('script');
     gtagScript.async = true;
